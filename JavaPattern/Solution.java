001ad974909a8f93461c15f0d984b4d11de49029
Vinay-Gole <119567736+Vinay-Gole@users.noreply.github.com>
2024-03-09 15:09:10 +0000

    pt5

--- JavaPattern/Solution.java
+++ JavaPattern/Solution.java
@@ -59,12 +59,12 @@ for(int i=1;i<n;i++){
 
   static void  patternfive(int n){
 
-    for(int i=1;i<n;i++){
-      for(int j){
-        System.out.print(" ");
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=n-i;j++){
+        System.out.print("   ");
       }
-      for(int k=1;k<;k++){
-        System.out.print("*");
+      for(int k=1;k<i;k++){
+        System.out.print(" "+"*"+" ");
       }
       System.out.println();
     }
